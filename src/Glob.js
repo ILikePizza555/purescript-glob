9a48dba819caf5e5248ea550dd84c1a629632e00
Izzy <avrisaac555@gmail.com>
2020-04-04 23:02:04 -0700

    Moved to using aff

--- src/Glob.js
+++ src/Glob.js
@@ -1,8 +1,8 @@
 "use strict";
 
-exports.globImpl = function(pattern, options, onError, onSuccess) {
-    return () => require("glob")(pattern, options, (error, matches) => {
-        if (error) onError(error)();
-        else onSuccess(matches)();
+exports.globImpl = function(pattern, options) {
+    return () => (onError, onSuccess) => require("glob")(pattern, options, (error, matches) => {
+        if (error) { onError(error); }
+        else { onSuccess(matches); }
     });
 }
--- src/Glob.purs
+++ src/Glob.purs
@@ -1,36 +1,18 @@
-module Glob (Pattern, glob, globCont) where
+module Glob (Pattern, glob) where
 
 import Prelude
 
-import Control.Monad.Cont.Trans (ContT(..))
-import Data.Either (Either(..))
-import Data.Function.Uncurried (Fn4, runFn4)
+import Data.Function.Uncurried (Fn2, runFn2)
 import Data.Options (Options, options)
-import Effect (Effect)
-import Foreign (Foreign, ForeignError)
+import Effect.Aff (Aff)
+import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
+import Foreign (Foreign) 
 import Glob.Options (GlobOptions)
 
 type Pattern = String
 
-type CallbackSignature = Either ForeignError (Array String)
-
 foreign import globImpl
-    :: Fn4 Pattern
-           Foreign
-           (ForeignError -> Effect Unit)
-           (Array String -> Effect Unit)
-           (Effect Unit)
+    :: Fn2 Pattern Foreign (EffectFnAff (Array String))
 
--- Normal glob function. Takes a glob patters, an options object, and a callback.
-glob :: Pattern 
-     -> Options GlobOptions 
-     -> (CallbackSignature -> Effect Unit) 
-     -> Effect Unit
-glob pattern opts k =
-    runFn4 globImpl pattern (options opts) (k <<< Left) (k <<< Right)
-
--- Glob function wrapped in the ContT monad transformer
-globCont :: Pattern
-         -> Options GlobOptions
-         -> ContT Unit Effect CallbackSignature
-globCont pattern opts = ContT $ glob pattern opts
+glob :: Pattern -> Options GlobOptions -> Aff (Array String)
+glob pattern opts = fromEffectFnAff $ runFn2 globImpl pattern (options opts)
