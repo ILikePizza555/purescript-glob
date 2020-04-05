dc8bc83251054b4991e1c31b7462968318dea8b4
Izzy <avrisaac555@gmail.com>
2020-04-04 22:32:32 -0700

    Added glob and globCont

--- src/Glob.purs
+++ src/Glob.purs
@@ -1,11 +1,18 @@
-module Glob (Pattern, globImpl) where
+module Glob (Pattern, glob, globCont) where
+
 import Prelude
 
-import Data.Function.Uncurried (Fn4)
+import Control.Monad.Cont.Trans (ContT(..))
+import Data.Either (Either(..))
+import Data.Function.Uncurried (Fn4, runFn4)
+import Data.Options (Options, options)
 import Effect (Effect)
 import Foreign (Foreign, ForeignError)
+import Glob.Options (GlobOptions)
 
 type Pattern = String
+
+type CallbackSignature = Either ForeignError (Array String)
 
 foreign import globImpl
     :: Fn4 Pattern
@@ -14,4 +21,16 @@ foreign import globImpl
            (Array String -> Effect Unit)
            (Effect Unit)
 
-glob :: forall eff. Pattern -> Options GlobOptions
+-- Normal glob function. Takes a glob patters, an options object, and a callback.
+glob :: Pattern 
+     -> Options GlobOptions 
+     -> (CallbackSignature -> Effect Unit) 
+     -> Effect Unit
+glob pattern opts k =
+    runFn4 globImpl pattern (options opts) (k <<< Left) (k <<< Right)
+
+-- Glob function wrapped in the ContT monad transformer
+globCont :: Pattern
+         -> Options GlobOptions
+         -> ContT Unit Effect CallbackSignature
+globCont pattern opts = ContT $ glob pattern opts
