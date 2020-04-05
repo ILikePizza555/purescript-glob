677c2e6b395d729d9e95a354937983ce3bba1fd3
Izzy <avrisaac555@gmail.com>
2020-04-04 23:52:22 -0700

    Added some unit tests

--- test/Main.purs
+++ test/Main.purs
@@ -3,13 +3,25 @@ module Test.Main where
 import Glob
 import Prelude
 
-import Data.Function.Uncurried (runFn4)
+import Data.Options (Options(..), (:=))
 import Effect (Effect)
+import Effect.Aff (Fiber, launchAff_)
 import Effect.Class.Console (log)
-import Foreign (unsafeToForeign)
+import Glob.Options (dot, nodir)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = do
-  log "Testing impl"
-  
-  runFn4 globImpl "*" (unsafeToForeign unit) (log <<< show) (log <<< show)
+main = launchAff_ $ runSpec [consoleReporter] do
+    describe "glob" do
+        it "runs a wildcard with no options" do
+            result <- glob "*" (Options [])
+            result `shouldEqual` ["node_modules","output","package-lock.json","packages.dhall","spago.dhall","src","test"]
+        it "can pass options" do
+            result <- glob "*.@(gitignore|json|dhall)" $ 
+                dot := true <>
+                nodir := true
+            log <<< show $ result
+            result `shouldEqual` [".gitignore", "package-lock.json", "packages.dhall", "spago.dhall"]
